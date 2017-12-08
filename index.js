28fd525a2c23d6ddf01ff54ed77655fe6ffb07c1
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-12-08 19:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = "I don't do much."
 function destructivelyAppendKitten(kittens){
   var kittens = ["Milo", "Otis", "Garfield"]
   kittens = kittens.push("Ralph");
-  return kittens
+  destructivelyAppendKitten()
 }
