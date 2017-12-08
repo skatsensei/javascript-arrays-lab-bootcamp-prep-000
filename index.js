19392ed498ca4a614809ebde66c11f02f157a6e7
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-12-08 20:44:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function destructivelyAppendKitten(name){
   kittens.push(name)
   return kittens 
 }
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens 
+}
+destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
